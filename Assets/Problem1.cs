45350e2bf1feeff1ead0c5ecddb07c5044b57a79
Zesty <WrGraban@gmail.com>
2016-02-29 17:57:42 -0800

    Solution 1 Complete
    I can add some extra notes about the commit here if I wanted to.

---
+++ Assets/Problem1.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Problem1 : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        /*
+            If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+            The sum of these multiples is 23.
+
+            Find the sum of all the multiples of 3 or 5 below 1000.
+        */
+        int sum = 0;
+
+        for(int num = 0; num < 1000; ++num)
+        {
+            if(num % 3 == 0 || num % 5 == 0)
+            {
+                sum += num;
+            }
+        }
+
+        Debug.Log("Problem 1 Solution: " + sum);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
